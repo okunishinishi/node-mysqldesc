34bc68ec18657420bc7cac96a632c97c91f2ea0d
Taka Okunishi <okunishinishi@gmail.com>
2015-07-26 06:54:10 +0900

    Modify test

--- test/mysql_describer_test.js
+++ test/mysql_describer_test.js
@@ -47,38 +47,11 @@ exports['Describe key column usage.'] = function (test) {
     var describer = new MysqlDescriber(testDbConfig);
     describer.describeDatabaseKeyColumnUsage('descmysql_test', function (err, data) {
         test.ifError(err);
-        test.deepEqual(data, {
-            TEST_PERSON: {},
-            TEST_PRODUCT: {
-                id: {
-                    CONSTRAINT_CATALOG: 'def',
-                    CONSTRAINT_SCHEMA: 'descmysql_test',
-                    CONSTRAINT_NAME: 'PRIMARY',
-                    TABLE_CATALOG: 'def',
-                    ORDINAL_POSITION: 1
-                },
-                shop_id: {
-                    CONSTRAINT_CATALOG: 'def',
-                    CONSTRAINT_SCHEMA: 'descmysql_test',
-                    CONSTRAINT_NAME: 'test_product_ibfk_1',
-                    TABLE_CATALOG: 'def',
-                    ORDINAL_POSITION: 1,
-                    POSITION_IN_UNIQUE_CONSTRAINT: 1,
-                    REFERENCED_TABLE_SCHEMA: 'descmysql_test',
-                    REFERENCED_TABLE_NAME: 'TEST_SHOP',
-                    REFERENCED_COLUMN_NAME: 'id'
-                }
-            },
-            TEST_SHOP: {
-                id: {
-                    CONSTRAINT_CATALOG: 'def',
-                    CONSTRAINT_SCHEMA: 'descmysql_test',
-                    CONSTRAINT_NAME: 'PRIMARY',
-                    TABLE_CATALOG: 'def',
-                    ORDINAL_POSITION: 1
-                }
-            }
-        });
+        test.deepEqual(Object.keys(data), [
+            "TEST_PERSON",
+            "TEST_PRODUCT",
+            "TEST_SHOP"
+        ]);
         test.done();
     });
 };
